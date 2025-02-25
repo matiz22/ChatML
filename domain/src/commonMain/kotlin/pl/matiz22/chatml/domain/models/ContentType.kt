0b777295e5ff7e8d519a8bd49d34890a22be3214
matiz22 <mateuszmalich@gmail.com>
2025-02-25 17:14:36 +0100

    adding values to content types

--- domain/src/commonMain/kotlin/pl/matiz22/chatml/domain/models/ContentType.kt
+++ domain/src/commonMain/kotlin/pl/matiz22/chatml/domain/models/ContentType.kt
@@ -1,6 +1,8 @@
 package pl.matiz22.chatml.domain.models
 
-enum class ContentType {
-    TEXT,
-    IMAGE_URL,
+enum class ContentType(
+    val value: String,
+) {
+    TEXT("text"),
+    IMAGE_URL("image_url"),
 }

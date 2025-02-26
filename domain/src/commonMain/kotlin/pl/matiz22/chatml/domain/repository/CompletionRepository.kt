ff8117eb638ad1874c82694d5ef1f458d5dd688a
matiz22 <mateuszmalich@gmail.com>
2025-02-26 20:52:48 +0100

    moving to flow

--- domain/src/commonMain/kotlin/pl/matiz22/chatml/domain/repository/CompletionRepository.kt
+++ domain/src/commonMain/kotlin/pl/matiz22/chatml/domain/repository/CompletionRepository.kt
@@ -1,11 +1,14 @@
 package pl.matiz22.chatml.domain.repository
 
+import kotlinx.coroutines.flow.Flow
 import pl.matiz22.chatml.domain.models.ChatResponse
+import pl.matiz22.chatml.domain.models.CompletionOptions
 import pl.matiz22.chatml.domain.models.Message
 
 interface CompletionRepository {
-    fun completion(
+    suspend fun completion(
         model: String,
         messages: List<Message>,
-    ): ChatResponse
+        options: CompletionOptions,
+    ): Flow<ChatResponse>
 }

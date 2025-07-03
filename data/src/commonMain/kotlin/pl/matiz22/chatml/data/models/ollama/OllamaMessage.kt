df32eab01f08a23d2dd32f8785f588936e097697
matiz22 <mateuszmalich@gmail.com>
2025-07-03 16:07:49 +0200

    Ollama models

---
+++ data/src/commonMain/kotlin/pl/matiz22/chatml/data/models/ollama/OllamaMessage.kt
@@ -0,0 +1,56 @@
+package pl.matiz22.chatml.data.models.ollama
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import pl.matiz22.chatml.domain.models.Content
+import pl.matiz22.chatml.domain.models.Message
+import pl.matiz22.chatml.domain.models.Role
+
+@Serializable
+data class OllamaMessage(
+    @SerialName("content")
+    val content: String,
+    @SerialName("images")
+    val images: List<String>? = null,
+    @SerialName("role")
+    val role: String,
+) {
+    internal fun <T> toMessages(serializer: KSerializer<T>): List<Message> {
+        val textContent = Json.decodeFromString(serializer, this.content)
+        val images =
+            this.images?.map { img ->
+                Message(
+                    role = Role.valueOf(this.role),
+                    content = Content.Image(url = img),
+                )
+            }
+        return listOf(
+            Message(
+                content = Content.Tool(value = textContent),
+                role = Role.valueOf(this.role.uppercase()),
+            ),
+            *(images?.toTypedArray() ?: emptyArray()),
+        )
+    }
+
+    internal fun toMessages(): List<Message> {
+        val textContent =
+            Message(
+                content = Content.Text(text = this.content),
+                role = Role.valueOf(this.role.uppercase()),
+            )
+        val images =
+            this.images?.map { img ->
+                Message(
+                    role = Role.valueOf(this.role),
+                    content = Content.Image(url = img),
+                )
+            }
+        return listOf(
+            textContent,
+            *(images?.toTypedArray() ?: emptyArray()),
+        )
+    }
+}
---
+++ data/src/commonMain/kotlin/pl/matiz22/chatml/data/models/ollama/OllamaRequest.kt
@@ -0,0 +1,18 @@
+package pl.matiz22.chatml.data.models.ollama
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
+
+@Serializable
+data class OllamaRequest(
+    @SerialName("messages")
+    val messages: List<OllamaMessage>,
+    @SerialName("model")
+    val model: String,
+    @SerialName("options")
+    val options: OllamaOptions,
+    @SerialName("stream")
+    val stream: Boolean,
+    val format: JsonElement? = null,
+)
